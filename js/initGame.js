668874412115eb05c7cd6e734f9bc2e304b48999
Victor Tang <vtange@users.noreply.github.com>
2018-10-04 23:14:35 -0700

    drag practice

---
+++ js/initGame.js
@@ -0,0 +1,29 @@
+/**
+* Load the scene when the canvas is fully loaded
+*/
+document.addEventListener("DOMContentLoaded", function () {
+	var strDivID = "BABYLON_GAME";
+	var canvas = document.getElementById(strDivID);
+	var support = BABYLON.Engine.isSupported();
+
+    if (support && canvas) {
+
+		var game_core = new Scene(canvas);
+		game_core.loadMap(library_f2);
+    }
+	else
+	{
+		//write non-supported error on #BABYLON_GAME
+	}
+	var svg = document.getElementById("svg");
+	const svgdims = svg.getBoundingClientRect();
+	const svgTop = svgdims.top;
+	const svgLef = svgdims.left;
+
+	d3.select(svg).call(
+		d3.drag()
+		.on("start",function(){console.log("start")})
+		.on("drag",function(){console.log("drag",d3.event.x-svgLef,d3.event.y-svgTop)})
+		.on("end",function(){console.log("end")})
+	);
+}, false);
