dc144312cf580912277f41caefab4963234adc18
Victor Tang <vtange@users.noreply.github.com>
2018-10-09 23:11:22 -0700

    remove bjs

--- js/initGame.js
+++ js/initGame.js
@@ -2,21 +2,6 @@
 * Load the scene when the canvas is fully loaded
 */
 document.addEventListener("DOMContentLoaded", function () {
-	var strDivID = "BABYLON_GAME";
-	var canvas = document.getElementById(strDivID);
-	var support = BABYLON.Engine.isSupported();
-
-    if (support && canvas) {
-
-		var game_core = new Scene(canvas);
-		//game_core.loadMap(library_f2);
-    }
-	else
-	{
-		//write non-supported error on #BABYLON_GAME
-	}
-
-
 	////////////
 	//touchpad//
 	////////////
