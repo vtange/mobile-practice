4cd6de7d46c47fca943dd2ee1a598c02c86662e5
Victor Tang <vtange@users.noreply.github.com>
2018-12-18 23:33:26 -0800

    filelist

--- sw-test/sw.js
+++ sw-test/sw.js
@@ -1,11 +1,54 @@
-console.log('Started', self);
+const cacheName = 'v1';
+const cacheFiles = [
+    '/',
+    '/css/style.css',
+    '/js/initGame.js',
+    '/lib/d3-dispatch.v1.min.js',
+    '/lib/d3-drag.v1.min.js',
+    '/lib/d3-selection.v1.min.js',
+]
+
+// Install event
 self.addEventListener('install', function(event) {
-  self.skipWaiting();
-  console.log('Installed', event);
+    console.log("SW installed");
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(function(cache){
+            console.log('SW caching cachefiles');
+            return cache.addAll(cacheFiles);
+        })
+    )
 });
+
+// Activate event
 self.addEventListener('activate', function(event) {
-console.log('Activated', event);
+    console.log("SW activated");
+    event.waitUntil(
+        caches.keys()
+        .then(function(cacheNames){
+            return Promise.all(cacheNames.map(function(thisCacheName){
+                if(thisCacheName !== cacheName){
+                    console.log('SW Removing cached files from', thisCacheName);
+                    return caches.delete(thisCacheName);
+                }
+            }))
+        })
+    )
 });
+
+// Push event
 self.addEventListener('push', function(event) {
   console.log('Push message received', event);
+});
+
+// Fetch event
+self.addEventListener('fetch', function(event) {
+    console.log("SW fetching", event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response){
+            console.log('Fetching new files');
+            return response || fetch(event.request);
+        })
+    );
 });
